bdfb0fd120ecab4bcc991c8dc95e7b0dcb2439a2
Pheonixork <tasorkpheonix@gmail.com>
2021-10-23 02:11:06 +0300

    fstcmt

---
+++ src/pages/step-page.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import styled from "styled-components"
+
+//styles
+const PageStyles = styled.div()`
+  color: darkgray;
+  margin: auto;
+`
+
+const MainContainer = styled.section()`
+  color: red;
+  margin: auto;
+`
