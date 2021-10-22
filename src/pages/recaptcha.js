ca017b85ec127e6b38be95dc36e6af22250c1885
pheonixork <92811664+pheonixork@users.noreply.github.com>
2021-10-23 02:42:51 +0300

    Update recaptcha.js

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -13,16 +13,6 @@ if (typeof RECAPTCHA_KEY === 'undefined') {
   `)
 }
 
-const RECAPTCHA_KEY2 = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
-if (typeof RECAPTCHA_KEY2 === 'undefined') {
-  throw new Error(`
-  Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
-  You probably forget to set it in your Netlify build environment variables. 
-  Make sure to get a Recaptcha key at https://www.netlify.com/docs/form-handling/#custom-recaptcha-2-with-your-own-settings
-  Note this demo is specifically for Recaptcha v2
-  `)
-}
-
 function encode(data) {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
